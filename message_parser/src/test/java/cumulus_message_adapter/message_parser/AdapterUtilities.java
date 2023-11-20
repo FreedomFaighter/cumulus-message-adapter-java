14cbddbdf2fd48d83b772c7a78d881e9b281142d
John H. Freeman <john@freemania.pro>
2023-11-19 21:41:08 -0500

    Update AdapterUtilities.java (#2)

--- message_parser/src/test/java/cumulus_message_adapter/message_parser/AdapterUtilities.java
+++ message_parser/src/test/java/cumulus_message_adapter/message_parser/AdapterUtilities.java
@@ -57,7 +57,7 @@ public class AdapterUtilities {
      */
     private static String getJsonResponse(String url) throws IOException {
         URL requestUrl = new URL(url);
-        HttpURLConnection conn = (HttpURLConnection) requestUrl.openConnection()
+        HttpURLConnection conn = (HttpURLConnection) requestUrl.openConnection();
         if (conn.getResponseCode() != HttpURLConnection.HTTP_OK) {
             throw new RuntimeException("Request Failed. HTTP Error Code: " + conn.getResponseCode());
         }
